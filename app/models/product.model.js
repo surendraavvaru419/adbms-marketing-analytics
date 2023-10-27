c93c7415237b28aefe79d8495dc24032ea005296
Naga Venkata Gopi Prudhvi Maddina <prudhvi@Nagas-MacBook-Air.local>
2023-10-27 02:40:39 -0500

    Added create, update, delete and deleteAll api's in product

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -7,6 +7,19 @@ const Product = function(product) {
   this.price = product.price;
 };
 
+Product.create = (newProduct, result) => {
+  sql.query("INSERT INTO products SET ?", newProduct, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created product: ", { id: res.insertId, ...newProduct });
+    result(null, { id: res.insertId, ...newProduct });
+  });
+};
+
 Product.findById = (id, result) => {
   sql.query(`SELECT * FROM products WHERE id = ${id}`, (err, res) => {
     if (err) {
@@ -41,6 +54,61 @@ Product.getAll = (title, result) => {
     }
 
     console.log("products: ", res);
+    result(null, res);
+  });
+};
+
+Product.updateById = (id, product, result) => {
+  sql.query(
+    "UPDATE products SET title = ?, description = ?, price = ? WHERE id = ?",
+    [product.title, product.description, product.price, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Product with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated product: ", { id: id, ...product });
+      result(null, { id: id, ...product });
+    }
+  );
+};
+
+Product.remove = (id, result) => {
+  sql.query("DELETE FROM products WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Product with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted product with id: ", id);
+    result(null, res);
+  });
+};
+
+Product.removeAll = result => {
+  sql.query("DELETE FROM products", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} products`);
     result(null, res);
   });
 };
--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,13 +1,30 @@
 module.exports = app => {
     const products = require("../controllers/product.controller.js");
   
-    var router = require("express").Router();
+    const express = require('express');
+
+    var router = express.Router();
+
+    router.use(express.json());
 
     // Retrieve all products
     router.get("/", products.findAll);
   
     // Retrieve a single product with id
     router.get("/:id", products.findOne);
+
+    // Create and save new Product
+    router.post("/", products.create);
+
+    // Update a Product with id
+    router.put("/:id", products.update);
+
+    // Delete a Product with id
+    router.delete("/:id", products.delete);
+
+    // Delete all Products
+    router.delete("/", products.deleteAll);
+
 
     app.use('/api/products', router);
 };
